45182ca68ac82302221e0ef504f945726f52c7a6
Sebastian Lennström <sebastian.lennstrom1@gmail.com>
2023-01-09 14:28:22 +0100

    changed in docs and renderGalleryItem

--- Client/script.js
+++ Client/script.js
@@ -1,8 +1,9 @@
-const listElement = document.getElementById("gallery__list")
+const listElement = document.getElementById("gallery__list");
 
 
 const galleryApi = new GalleryApi('http://localhost:5000/images');
 
+let gallery = [];
 
 
 
@@ -17,7 +18,7 @@ function renderGallery(){
             
           if (gallery && gallery.length > 0) {
             gallery.forEach((Item) => {
-              listElement.insertAdjacentHTML('beforeend', renderGalleryItem(Item));
+              listElement.insertAdjacentHTML('beforeend', renderGalleryItem(Item))
             });
           }
         });
@@ -29,25 +30,38 @@ function renderGallery(){
 
 
 
-
+//Bör lägga till funktioner för att ta med id vid visning av bilden
 function renderGalleryItem({name, description, image, alt}){
 
     
     let html = `
         <li class="flex w-2/5  mt-2 p-3">
-            <div class="flex flex-col">
+            <div class="flex flex-row">
                 <div class="flex items-center justify-start flex-col">
                     <h3 class="mb-1 text-xl font-bold text-black uppercase">${name}</h3>
-                    <img class= "w-3/5" src="${image}" alt="${alt}">
+                    <img class= "w-4/5" src="${image}" alt="${alt}">
                 </div>
                 <br>
-                <p class="m-10 text-xs text-black self-center">${description}</p>
-            </div>
+                <div class="flex flex-row">
+                  <p class="m-3 text-s text-black">${description}</p>
+                  <button name="showItem" class="rounded-md bg-blue-300 hover:bg-blue-100 px-4 py-1 w-1/5 self-end" type="submit" onclick="showItem(${name})"> Visa </button>
+                </div>
+              </div>
         </li>`;
   
     return html;
   }
 
+
+
+function getPictureName(name){
+  for (let i = 0; i < gallery.length; i++) {
+    if(gallery[i].name == name){
+    console.log(i)
+    return gallery[i]
+    }
+  }
+}
 
 renderGallery();
 
