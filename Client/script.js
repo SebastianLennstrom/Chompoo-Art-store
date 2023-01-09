25db1e0e73a4f5ac8a3faadee8fdbe755b6d6e30
Sebastian Lennström <sebastian.lennstrom1@gmail.com>
2023-01-09 11:14:58 +0100

    works to get all images and print gallery

--- Client/script.js
+++ Client/script.js
@@ -18,7 +18,6 @@ function renderGallery(){
           if (gallery && gallery.length > 0) {
             gallery.forEach((Item) => {
               listElement.insertAdjacentHTML('beforeend', renderGalleryItem(Item));
-              console.log(renderGalleryItem(Item))
             });
           }
         });
@@ -31,17 +30,18 @@ function renderGallery(){
 
 
 
-function renderGalleryItem(name, description, image, alt){
+function renderGalleryItem({name, description, image, alt}){
 
     
     let html = `
-        <li class="flex bg-white bg-opacity-75 mt-2 p-3 rounded border-neutral-400 border border-solid shadow">
+        <li class="flex w-2/5 bg-white bg-opacity-75 mt-2 p-3 rounded border-neutral-400 border border-solid shadow">
             <div class="flex-1">
-                <div class="flex items-center justify-start">
-                    <h3 class="mb-1 text-xl font-bold text-pink-800 uppercase">${name}</h3>
-                    <img src="${image}" alt="${alt}">
+                <div class="flex items-center justify-start flex-col">
+                    <h3 class="mb-1 text-xl font-bold text-black shadow-None uppercase">${name}</h3>
+                    <img class= "w-3/5" src="${image}" alt="${alt}">
                 </div>
-                <p class="mt-2 text-xs">${description}</p>
+                <br>
+                <p class="mt-2 text-xs text-black">${description}</p>
             </div>
         </li>`;
   
