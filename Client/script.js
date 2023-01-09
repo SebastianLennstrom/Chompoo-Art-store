c485e2474ecfbce26026f944eb09f989603d4f96
Sebastian Lennström <sebastian.lennstrom1@gmail.com>
2023-01-09 12:33:53 +0100

    changed css

--- Client/script.js
+++ Client/script.js
@@ -34,14 +34,14 @@ function renderGalleryItem({name, description, image, alt}){
 
     
     let html = `
-        <li class="flex w-2/5 bg-white bg-opacity-75 mt-2 p-3 rounded border-neutral-400 border border-solid shadow">
-            <div class="flex-1">
+        <li class="flex w-2/5  mt-2 p-3">
+            <div class="flex flex-col">
                 <div class="flex items-center justify-start flex-col">
-                    <h3 class="mb-1 text-xl font-bold text-black shadow-None uppercase">${name}</h3>
+                    <h3 class="mb-1 text-xl font-bold text-black uppercase">${name}</h3>
                     <img class= "w-3/5" src="${image}" alt="${alt}">
                 </div>
                 <br>
-                <p class="mt-2 text-xs text-black">${description}</p>
+                <p class="m-10 text-xs text-black self-center">${description}</p>
             </div>
         </li>`;
   
